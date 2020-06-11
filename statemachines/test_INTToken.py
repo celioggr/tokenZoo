5493158a04152f35c745ce2236eceae2011c5cec
celioggr <celioggr@gmail.com>
2020-06-11 22:59:44 +0100

    test statemachines for INTToken and SwiftCoin

---
+++ statemachines/test_INTToken.py
@@ -0,0 +1,44 @@
+from statemachine import StateMachine
+import brownie,pytest
+from brownie import project
+
+@pytest.fixture()
+def contract2test():
+    token = project.load('/home/honeybadger/2test').INT
+    yield token
+
+class INT(StateMachine):
+    def __init__(self,accounts,contract2test):
+        super().__init__(self,accounts,contract2test)
+        self.totalSupply = 1000000000000000
+        self.contract = contract2test.deploy({"from": accounts[0]})
+
+    def rule_mint(self,st_receiver,st_minter,st_amount):
+        if self.contract.owner():
+            self.contract.mintToken(st_receiver,st_amount,{"from":st_minter})
+            self.totalSupply += st_amount
+            self.balances[st_receiver] += st_amount
+        else:
+            with brownie.reverts():
+                self.contract.mintToken(st_receiver,st_amount,{"from":st_minter})
+
+    def rule_totalSupply_overflows(self,st_minter,st_receiver):
+        uint256max = 2**256 - 1
+        if self.contract.owner():
+            tx = self.contract.mintToken(st_receiver,uint256max,{"from":st_minter})
+            self.totalSupply += uint256max
+            self.balances[st_receiver] += uint256max
+        else:
+            with brownie.reverts():
+                self.contract.mintToken(st_receiver,uint256max,{"from":st_minter})
+
+    def rule_decrease_the_allowance(self, st_spender, st_owner,st_amount):
+        pass
+
+    def rule_increase_the_allowance(self, st_spender, st_owner,st_amount):
+        pass
+
+
+def test_stateful(contract2test,accounts,state_machine):
+    settings = {"stateful_step_count": 10, "max_examples": 40, "report_multiple_bugs":True,"derandomize":False,"database":None, "print_blob":True}
+    state_machine(BitAseanToken,accounts,contract2test,settings=settings)
---
+++ statemachines/test_SwiftCoin.py
@@ -0,0 +1,44 @@
+from statemachine import StateMachine
+import brownie,pytest
+from brownie import project
+
+@pytest.fixture()
+def contract2test():
+    token = project.load('/home/honeybadger/2test').SwftCoin
+    yield token
+
+class SwftCoin(StateMachine):
+    def __init__(self,accounts,contract2test):
+        super().__init__(self,accounts,contract2test)
+        self.totalSupply = 1000
+        self.contract = contract2test.deploy(1000,"SwiftCoin",18,"SFC",{"from": accounts[0]})
+
+    def rule_mint(self,st_receiver,st_minter,st_amount):
+        if self.contract.owner():
+            self.contract.mintToken(st_receiver,st_amount,{"from":st_minter})
+            self.totalSupply += st_amount
+            self.balances[st_receiver] += st_amount
+        else:
+            with brownie.reverts():
+                self.contract.mintToken(st_receiver,st_amount,{"from":st_minter})
+
+    def rule_totalSupply_overflows(self,st_minter,st_receiver):
+        uint256max = 2**256 - 1
+        if self.contract.owner():
+            tx = self.contract.mintToken(st_receiver,uint256max,{"from":st_minter})
+            self.totalSupply += uint256max
+            self.balances[st_receiver] += uint256max
+        else:
+            with brownie.reverts():
+                self.contract.mintToken(st_receiver,uint256max,{"from":st_minter})
+
+    def rule_decrease_the_allowance(self, st_spender, st_owner,st_amount):
+        pass
+
+    def rule_increase_the_allowance(self, st_spender, st_owner,st_amount):
+        pass
+
+
+def test_stateful(contract2test,accounts,state_machine):
+    settings = {"stateful_step_count": 10, "max_examples": 40, "report_multiple_bugs":True,"derandomize":False,"database":None, "print_blob":True}
+    state_machine(SwftCoin,accounts,contract2test,settings=settings)
